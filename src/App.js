c8fc378be174d307cf8a07e82a358107d3a73aa0
Mariana Morais <mariana.ds.morais04@gmail.com>
2022-06-23 18:43:09 +0100

    created weather app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="infoday">
+      <span className="description" id="description">
+        Clear
+      </span>
+      <span className="day" id="currentDate">
+        Sunday, 13h33
+      </span>
+      <span className="update">updated 7 mins ago</span>
     </div>
   );
 }
-
-export default App;
